e9d3a2ad432b65f016463962d4c8ef702fef5a05
sina-byn <sina.sina.bayan@protonmail.com>
2024-11-20 10:24:51 +0330

    fix: search functionality bug fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const searchInput = document.querySelector('input');
 const logs = [...document.querySelectorAll('.logs > a')];
 
-searchInput.onchange = e => {
-  const { value } = e.target;
+searchInput.oninput = e => {
+  const value = e.target.value.toLowerCase();
 
   logs.forEach(log => {
-    const hasQuery = log.textContent.includes(value);
+    const hasQuery = log.textContent.toLowerCase().includes(value);
     log.style.display = hasQuery ? 'block' : 'none';
   });
 };
