fada8f94c6137628074e284dd2d8e6d7ca14d73b
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-02 18:58:36 +0330

    refactor: logGPT func refactored to replace the GET logic with the new extractChatData func

--- src/index.js
+++ src/index.js
@@ -19,12 +19,9 @@ const converter = new showdown.Converter();
 converter.setFlavor('github');
 
 // * utils
-const { GET } = require('./utils');
+const { extractChatData } = require('./utils');
 const log = require('./utils/log');
 const filter = require('./utils/filter');
-
-const notFoundRegex = /404 Not Found/gi;
-const remixContextRegex = /window\.__remixContext\s*=\s*({[\s\S]*?});__remixContext.p/g;
 
 const updateLogIndex = async (title, fileName) => {
   log.info('Updating chat index...');
@@ -49,48 +46,18 @@ const updateLogIndex = async (title, fileName) => {
 
 const logGPT = async url => {
   log.info('Extracting chat data...');
-  const gptHtml = await GET(url);
-  if (notFoundRegex.test(gptHtml)) throw new Error('404 Not Found');
+  let [title, html] = await extractChatData(url);
+
+  if (title === '404') throw new Error('404 Not Found');
   log.success('Chat data extracted successfully!!!');
 
-  const __html = remixContextRegex.exec(gptHtml)[1];
-
-  const remixContext = JSON.parse(__html);
-  const data = remixContext.state.loaderData['routes/share.$shareId.($action)'].serverResponse.data;
-  const { title, linear_conversation } = data;
-  const conversation = linear_conversation.slice(2);
-  let html = '';
-
   log.warn('Chat Title:', title);
+  log.info("Generating chat's HTML...");
   const header = document.querySelector('header');
   const heading = document.createElement('h1');
 
   heading.textContent = title;
   header.append(heading);
-
-  for (const { message } of conversation) {
-    const { author, content } = message;
-    const { parts, content_type } = content;
-    const { role } = author;
-
-    if (content_type !== 'text') continue;
-
-    const bubble = document.createElement('div');
-    bubble.className = 'bubble';
-
-    const isUser = role === 'user';
-
-    if (isUser) {
-      bubble.classList.add('user');
-      const bubbleText = parts.join('');
-      bubble.textContent = bubbleText;
-    } else {
-      const bubbleHtml = converter.makeHtml(parts.join(''));
-      bubble.innerHTML = bubbleHtml;
-    }
-
-    html += bubble.outerHTML;
-  }
 
   const content = document.querySelector('.content');
   content.innerHTML = html;
@@ -99,7 +66,6 @@ const logGPT = async url => {
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
   const fileName = `${timestamp}.html`;
 
-  log.info("Generating chat's HTML...");
   fs.writeFileSync(fileName, html, 'utf-8');
   log.success("Chat's HTML was generated successfully");
 
